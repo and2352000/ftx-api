e97c972c25ba0bb22e4410177b218ccf21d16b1f
aaron haung <and2352000@gmail.com>
2021-11-17 23:25:40 +0800

    refactor: remove dist folder

--- dist/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HttpMethod = void 0;
-var HttpMethod;
-(function (HttpMethod) {
-    HttpMethod["GET"] = "get";
-    HttpMethod["POST"] = "post";
-})(HttpMethod = exports.HttpMethod || (exports.HttpMethod = {}));
-//# sourceMappingURL=types.js.map
